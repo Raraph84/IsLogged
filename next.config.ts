8be77b9727dd9ad0ff9dea6d38249c9bf111f758
Raraph84 <contact@raraph.fr>
2026-07-06 18:05:57 +0200

    Fix compilation and export to static build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  allowedDevOrigins: ["is.rlaurens.logged.fr"],
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
     title: "is.login.logged.fr"
@@ -11,7 +12,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                <Suspense>{children}</Suspense>
+            </body>
         </html>
     );
 }
